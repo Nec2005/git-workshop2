e74995f8f48e6f97c7b4fc0866af4e1ddb13038c
merveaydins <86128180+merveaydins@users.noreply.github.com>
2021-08-03 22:01:35 +0300

    ilk yorum

--- leap-year1.py
+++
@@ -1,5 +0,0 @@
-year = int(input("Enter a year :"))
-if (year%4 == 0 and year%100 != 0 ) or (year%100 == 0 and year%400 == 0):
-  print('This is a leap year')
-else :
-  print('This is not a leap year')
