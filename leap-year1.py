e750cfd23293708d7ba4b0cab4be9cd8740cb249
pakyasin23@gmail.com <87154797+yasint23@users.noreply.github.com>
2021-08-03 20:04:47 +0100

    elif line added

--- leap-year1.py
+++ leap-year1.py
@@ -1,5 +1,6 @@
 year = int(input("Enter a year :"))
 if (year%4 == 0 and year%100 != 0 ) or (year%100 == 0 and year%400 == 0):
   print('This is a leap year')
+  elif (year//4 ==0)
 else :
   print('This is not a leap year')
